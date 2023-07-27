fe94bb6587b7919f663d2bddc498ce1ec0c0675d
llecoq <lecoqlq@gmail.com>
2023-07-27 14:12:30 +0200

    add_sub_scale not codded

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Struct Matrix
+// Struct Vector
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod add_sub_scale;
