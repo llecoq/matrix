96db31a671034e3d7da08eb3e1397e8af54772c0
llecoq <lecoqlq@gmail.com>
2023-07-27 15:50:21 +0200

    Working on general structure of modules and types

---
+++ src/add_sub_scl.rs
@@ -0,0 +1,28 @@
+use crate::Matrix;
+use crate::Vector;
+
+// VECTOR
+impl<K> Vector<K> {
+    fn add(&mut self, v: &Vector<K>) {
+
+    }
+    fn sub(&mut self, v: &Vector<K>) {
+
+    }
+    fn scl(&mut self, a: K) {
+
+    }
+}
+
+// MATRIX 
+impl<K> Matrix<K> {
+    fn add(&mut self, v: &Matrix<K>){
+        
+    }
+    fn sub(&mut self, v: &Matrix<K>){
+        
+    }
+    fn scl(&mut self, a: K){
+        
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-// Struct Matrix
-// Struct Vector
+//---------------------------- TYPES
+pub struct Matrix<K> {
+    // utilisation des traits pour declarer les fonctions membres puis implementation dans des fichiers dedies
+}
+pub struct Vector<K> {
+    // utilisation des traits pour declarer les fonctions membres puis implementation dans des fichiers dedies
+}
 
-mod add_sub_scale;
+//---------------------------- MODULES
+mod add_sub_scl;
