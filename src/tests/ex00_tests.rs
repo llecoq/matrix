3716d0ad7c2979a27522bd1c27302957644631b1
llecoq <lecoqlq@gmail.com>
2023-08-03 19:48:06 +0200

    vector_to_string() test utility fn

--- src/tests/ex00_tests.rs
+++ src/tests/ex00_tests.rs
@@ -2,13 +2,42 @@ use crate::{Vector, traits::AddSubScl, Matrix};
 use std::fmt::Write as _;
 
 //--------------------------------------------------------------- Utility function
-fn  assert_vector_output<K>(vec: &Vector<K>, expected_output: &str)
+// assert vector and matrix output
+fn  assert_output<K>(struct_output: &K, expected_output: &str)
 where
     K: std::fmt::Display
 {
     let mut output = String::new();
-    writeln!(&mut output, "{}", vec).unwrap();
+    write!(&mut output, "{}", struct_output).unwrap();
     assert_eq!(output, expected_output);
+}
+
+// format vector to string
+fn  vector_to_string<K>(vector: &Vec<K>) -> String
+where
+    K: std::fmt::Display
+{
+    vector.iter()
+        .map(|vec| format!("[{:.1}]", vec))
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+// format matrix to string
+fn  matrix_to_string<K>(matrix: &Vec<Vec<K>>) -> String
+where
+    K: std::fmt::Display
+{
+    let flat_matrix: Vec<&K> = matrix
+        .into_iter()
+        .flatten()
+        .collect();
+
+    flat_matrix
+        .iter()
+        .map(|data| data.to_string())
+        .collect::<Vec<String>>()
+        .join("]")
 }
 
 
@@ -17,14 +46,16 @@ where
 // Vector
 #[test]
 fn  vector_utility_functions_tests() {
+    let input_v = vec![2., 6.3];
+    let input_v2: Vec<f64> = vec![];
     // from()
-    let v: Vector<f32> = Vector::from(vec![2., 6.3]);
-    let v2: Vector<f64> = Vector::from(vec![]);
+    let v: Vector<f32> = Vector::from(input_v.clone());
+    let v2: Vector<f64> = Vector::from(input_v2.clone());
     // get_size()
-    assert_eq!(v.get_size(), 2);
-    assert_eq!(v2.get_size(), 0);
+    assert_eq!(v.get_size(), input_v.len());
+    assert_eq!(v2.get_size(), input_v2.len());
     // print_vector()
-    assert_vector_output(&v, "[2.0][6.3]\n");
+    assert_output(&v, vector_to_string(&input_v).as_str());
     // reshape_into_matrix()
 
 }
@@ -36,10 +67,10 @@ fn  vector_add_tests() {
     let v3: Vector<f32> = Vector::from(vec![1.1, 2.]);
 
     v.add(&v3);
-    assert_vector_output(&v, "[3.1][8.3]\n");
+    assert_output(&v, "[3.1][8.3]\n");
 
     v.add(&v2); // should not do anything. Choosed not to use Result<> to make it lighter in use
-    assert_vector_output(&v, "[3.1][8.3]\n");
+    assert_output(&v, "[3.1][8.3]\n");
 }
 
 #[test]
@@ -49,10 +80,10 @@ fn  vector_sub_tests() {
     let v3: Vector<f32> = Vector::from(vec![1.1, 2.]);
 
     v.sub(&v3);
-    assert_vector_output(&v, "[0.9][4.3]\n");
+    assert_output(&v, "[0.9][4.3]\n");
 
     v.sub(&v2); // should not do anything. Choosed not to use Result<> to make it lighter in use
-    assert_vector_output(&v, "[0.9][4.3]\n");
+    assert_output(&v, "[0.9][4.3]\n");
 }
 
 #[test]
@@ -60,25 +91,30 @@ fn  vector_scl_tests() {
     let mut u = Vector::from(vec![2., 3.]);
 
     u.scl(2.);
-    assert_vector_output(&u, "[4.0][6.0]\n");
+    assert_output(&u, "[4.0][6.0]\n");
     u.scl(-1.);
-    assert_vector_output(&u, "[-4.0][-6.0]\n");
+    assert_output(&u, "[-4.0][-6.0]\n");
     u.scl(-0.);
-    assert_vector_output(&u, "[0.0][0.0]\n");
+    assert_output(&u, "[0.0][0.0]\n");
 }
 
 #[test]
 #[allow(unused_variables)]
 fn  matrix_utility_functions_tests() {
     // Valid
-    let matrix = Matrix::from(vec! [
+    let input = vec![
         vec![1.1, 2.],
         vec![1.1, 2.]
-    ]);
+    ];
+    let matrix = Matrix::from(input.clone());
     match matrix {
-        Ok(matrix) => println!("{}", matrix),        
+        Ok(matrix) => {
+            println!("{}", matrix);
+            assert_output(&matrix, matrix_to_string(&input).as_str());
+        }        
         Err(error) => println!("{}", error)
     };
+
 
     // Invalid matrix format
     let matrix = Matrix::from(vec! [
