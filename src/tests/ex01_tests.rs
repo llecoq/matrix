eeac1bfebf79a2204b733bb11c32c5912fc8f96c
llecoq <lecoqlq@gmail.com>
2023-08-15 13:02:52 +0200

    Update linear_combination

--- src/tests/ex01_tests.rs
+++ src/tests/ex01_tests.rs
@@ -19,4 +19,14 @@ fn linear_combination_tests() {
 
     assert_output(&linear_combination(&[e1, e2, e3], &[16., -2.6, 0.5]), "[-15.7][-273.0][-13.5]");
     assert_output(&linear_combination(&[v1, v2], &[-10., -2.]), "[-14.0][-50.0][1370.0]");
+
+    let e1 = Vector::from(vec![-1., 0., 0.]);
+    let e2 = Vector::from(vec![0., 5.]);
+    let e3 = Vector::from(vec![0.6, 0., -1.]);
+    let v1 = Vector::from(vec![1., 2., 3.]);
+    let v2 = Vector::from(vec![2., 15., -700.]);
+
+    // undefined
+    println!("{}", linear_combination(&[e1, e2, e3], &[16., 0.5]));
+    println!("{}", linear_combination(&[v1, v2], &[1., 0., 7., 9.]));
 }
