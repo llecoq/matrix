862f56ec7587c99323a7ba35a8df2c0f5ea484b5
llecoq <lecoqlq@gmail.com>
2023-08-15 12:11:25 +0200

    Assert output for linear_combination tests

--- src/tests/ex01_tests.rs
+++ src/tests/ex01_tests.rs
@@ -1,4 +1,4 @@
-use crate::{Vector, operations::linear_combination::linear_combination};
+use crate::{Vector, operations::linear_combination::linear_combination, tests::test_utils::assert_output};
 
 #[test]
 fn linear_combination_tests() {
@@ -8,7 +8,6 @@ fn linear_combination_tests() {
     let v1 = Vector::from(vec![1., 2., 3.]);
     let v2 = Vector::from(vec![0., 10., -100.]);
 
-
-    println!("{}", linear_combination(&[e1, e2, e3], &[10., -2., 0.5]));
-    println!("{}", linear_combination(&[v1, v2], &[10., -2.]));
+    assert_output(&linear_combination(&[e1, e2, e3], &[10., -2., 0.5]), "[10.0][-2.0][0.0]");
+    assert_output(&linear_combination(&[v1, v2], &[10., -2.]), "[10.0][0.0][230.0]");
 }
