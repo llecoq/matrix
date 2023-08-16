4043518f7f378730ddbc66264a316d8b2d455663
llecoq <lecoqlq@gmail.com>
2023-08-16 14:14:51 +0200

    Update dot tests

--- src/tests/ex03_tests.rs
+++ src/tests/ex03_tests.rs
@@ -4,14 +4,38 @@ use crate::Vector;
 fn dot_product_tests() {
     let u = Vector::from(vec![0., 0.]);
     let v = Vector::from(vec![1., 1.]);
-    println!("{}", u.dot(&v));
-    // 0.0
+    assert_eq!(u.dot(&v), 0.);
+
     let u = Vector::from(vec![1., 1.]);
     let v = Vector::from(vec![1., 1.]);
-    println!("{}", u.dot(&v));
-    // 2.0
+    assert_eq!(u.dot(&v), 2.);
+
     let u = Vector::from(vec![-1., 6.]);
     let v = Vector::from(vec![3., 2.]);
-    println!("{}", u.dot(&v));
-    // 9.0
+    assert_eq!(u.dot(&v), 9.);
+
+    let u = Vector::from(vec![-1., 6., 3., 9., 0., -9.]);
+    let v = Vector::from(vec![3., 2., 6., 3., -6., -1.]);
+    assert_eq!(u.dot(&v), 63.);
+
+    let u = Vector::from(vec![3., 9., 3., -9.]);
+    let v = Vector::from(vec![6., 3., -6., -1.]);
+    assert_eq!(u.dot(&v), 36.);
+
+    let u = Vector::from(vec![0., 0.]);
+    let v = Vector::from(vec![0., 0.]);
+    assert_eq!(u.dot(&v), 0.);
+
+    let u: Vector<f32> = Vector::from(vec![]);
+    let v = Vector::from(vec![]);
+    assert_eq!(u.dot(&v), 0.);
+
+    // undefined
+    let u: Vector<f32> = Vector::from(vec![1.]);
+    let v = Vector::from(vec![]);
+    assert_eq!(u.dot(&v), 0.);
+
+    let u: Vector<f32> = Vector::from(vec![]);
+    let v = Vector::from(vec![1.]);
+    assert_eq!(u.dot(&v), 0.);
 }
