ab55046ac835e9c4081558ef322440497a106f08
llecoq <lecoqlq@gmail.com>
2023-08-16 11:29:15 +0200

    Impl lerp()

---
+++ src/operations/linear_interpolation.rs
@@ -0,0 +1,17 @@
+use crate::traits::AddSubScl;
+
+/// Computes a linear interpolation between two objects of the same type.
+/// If the objects are not of the same size, extra items will be ignored
+#[allow(unused_variables)]
+pub fn lerp<V>(u: V, v: V, t: f32) -> V
+where
+    V:  AddSubScl<V, f32> + Clone
+{
+    let mut result: V = u.clone();
+    let mut v_clone: V = v.clone();
+
+    result.scl(1.0 - t);
+    v_clone.scl(t);
+    result.add(&v_clone);
+    result
+}
--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -1,5 +1,7 @@
 //---------------------- RE-EXPORT
 pub use linear_combination::linear_combination;
+pub use linear_interpolation::lerp;
 
 //----------------------- MODULES
 pub mod linear_combination;
+pub mod linear_interpolation;
