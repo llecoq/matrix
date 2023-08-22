c591d5b9593e06a0b8eb8f88ef8228fe14ab3b2f
llecoq <lecoqlq@gmail.com>
2023-08-22 17:01:54 +0200

    Update AddSubScl tests for Complex<K>

--- src/tests/ex00_tests.rs
+++ src/tests/ex00_tests.rs
@@ -83,6 +83,7 @@ fn  vector_add_tests() {
 
 #[test]
 fn  vector_sub_tests() {
+    //----------------------------------------------------------------------------------f32
     let mut v: Vector<f32> = Vector::from(vec![2., 6.3]);
     let v2: Vector<f32> = Vector::from(vec![]);
     let v3: Vector<f32> = Vector::from(vec![1.1, 2.]);
@@ -92,10 +93,22 @@ fn  vector_sub_tests() {
 
     v.sub(&v2); // should not do anything. Choosed not to use Result<> to make it lighter in use
     assert_output(&v, "[0.9][4.3]");
+
+    //---------------------------------------------------------------------------Complex<f32>
+    let mut v: Vector<Complex<f32>> = Vector::from(vec![Complex::new(2., 1.5), Complex::new(6.3, -2.4)]);
+    let v2: Vector<Complex<f32>> = Vector::from(vec![]);
+    let v3: Vector<Complex<f32>> = Vector::from(vec![Complex::new(1.1, -0.5), Complex::new(2., 2.4)]);
+    
+    v.sub(&v3);
+    assert_output(&v, "[0.9+2.0i][4.3-4.8i]");
+    
+    v.sub(&v2); // should not do anything. Chose not to use Result<> to make it lighter in use
+    assert_output(&v, "[0.9+2.0i][4.3-4.8i]");
 }
 
 #[test]
 fn  vector_scl_tests() {
+    //----------------------------------------------------------------------------------f32
     let mut u = Vector::from(vec![2., 3.]);
 
     u.scl(2.);
@@ -103,6 +116,18 @@ fn  vector_scl_tests() {
     u.scl(-1.);
     assert_output(&u, "[-4.0][-6.0]");
     u.scl(-0.);
+    assert_output(&u, "[0.0][0.0]");
+
+    //---------------------------------------------------------------------------Complex<f32>
+    let mut u: Vector<Complex<f32>> = Vector::from(vec![Complex::new(2., 1.5), Complex::new(3., -2.4)]);
+
+    u.scl(Complex::new(2., 0.));
+    assert_output(&u, "[4.0+3.0i][6.0-4.8i]");
+    
+    u.scl(Complex::new(-1., 0.));
+    assert_output(&u, "[-4.0-3.0i][-6.0+4.8i]");
+    
+    u.scl(Complex::new(0., 0.));
     assert_output(&u, "[0.0][0.0]");
 }
 
@@ -212,6 +237,7 @@ fn  matrix_utility_functions_tests() {
 
 #[test]
 fn matrix_add_sub_scl_tests() {
+    //--------------------------------------------------------------------------------f32
     let input = vec![
         vec![1.1, 2., 3.6],
         vec![2.1, 3., 2.6],
@@ -257,6 +283,55 @@ fn matrix_add_sub_scl_tests() {
         vec![4.2, 6.0, 5.2],
         vec![6.2, 4.0, 9.0],
         vec![8.6, 4.0, 9.0]
+    ];
+    assert_output(&matrix, matrix_to_string(&result).as_str());
+
+
+    //---------------------------------------------------------------------------Complex<f32>
+    let input = vec![
+        vec![Complex::new(1.1, 0.5), Complex::new(2., -0.5), Complex::new(3.6, 1.2)],
+        vec![Complex::new(2.1, 0.7), Complex::new(3., -1.0), Complex::new(2.6, 0.9)],
+        vec![Complex::new(3.1, -0.3), Complex::new(2., 0.5), Complex::new(4.5, -1.2)],
+        vec![Complex::new(4.3, 0.8), Complex::new(2., -0.8), Complex::new(4.5, 1.5)]
+    ];
+    let input_2 = vec![
+        vec![Complex::new(1., 0.), Complex::new(1., 0.), Complex::new(1., 0.)],
+        vec![Complex::new(1., 0.), Complex::new(1., 0.), Complex::new(1., 0.)],
+        vec![Complex::new(1., 0.), Complex::new(1., 0.), Complex::new(1., 0.)],
+        vec![Complex::new(1., 0.), Complex::new(1., 0.), Complex::new(1., 0.)]
+    ];
+    let matrix = Matrix::from(input.clone());
+    let matrix_2 = Matrix::from(input_2.clone());
+
+    //-------------------------------------------------------------------- add
+    let mut matrix: Matrix<Complex<f32>> = matrix.unwrap();
+    let matrix_2 = matrix_2.unwrap();
+    let result = vec![
+        vec![Complex::new(2.1, 0.5), Complex::new(3., -0.5), Complex::new(4.6, 1.2)],
+        vec![Complex::new(3.1, 0.7), Complex::new(4., -1.0), Complex::new(3.6, 0.9)],
+        vec![Complex::new(4.1, -0.3), Complex::new(3., 0.5), Complex::new(5.5, -1.2)],
+        vec![Complex::new(5.3, 0.8), Complex::new(3., -0.8), Complex::new(5.5, 1.5)]
+    ];
+    matrix.add(&matrix_2);
+    assert_output(&matrix, matrix_to_string(&result).as_str());
+
+    //-------------------------------------------------------------------- sub
+    matrix.sub(&matrix_2);
+    let result = vec![
+        vec![Complex::new(1.1, 0.5), Complex::new(2., -0.5), Complex::new(3.6, 1.2)],
+        vec![Complex::new(2.1, 0.7), Complex::new(3., -1.0), Complex::new(2.6, 0.9)],
+        vec![Complex::new(3.1, -0.3), Complex::new(2., 0.5), Complex::new(4.5, -1.2)],
+        vec![Complex::new(4.3, 0.8), Complex::new(2., -0.8), Complex::new(4.5, 1.5)]
+    ];
+    assert_output(&matrix, matrix_to_string(&result).as_str());
+
+    //-------------------------------------------------------------------- scale
+    matrix.scl(Complex::new(2., 0.));
+    let result = vec![
+        vec![Complex::new(2.2, 1.0), Complex::new(4., -1.0), Complex::new(7.2, 2.4)],
+        vec![Complex::new(4.2, 1.4), Complex::new(6., -2.0), Complex::new(5.2, 1.8)],
+        vec![Complex::new(6.2, -0.6), Complex::new(4., 1.0), Complex::new(9., -2.4)],
+        vec![Complex::new(8.6, 1.6), Complex::new(4., -1.6), Complex::new(9., 3.0)]
     ];
     assert_output(&matrix, matrix_to_string(&result).as_str());
 
