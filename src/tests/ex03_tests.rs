f4228673135dc0587f0cc4499e12bd9cac05cc09
llecoq <lecoqlq@gmail.com>
2023-08-22 19:08:24 +0200

    Update tests

--- src/tests/ex03_tests.rs
+++ src/tests/ex03_tests.rs
@@ -1,3 +1,5 @@
+use num_complex::Complex;
+
 use crate::Vector;
 
 #[test]
@@ -38,4 +40,43 @@ fn dot_product_tests() {
     let u: Vector<f32> = Vector::from(vec![]);
     let v = Vector::from(vec![1.]);
     assert_eq!(u.dot(&v), 0.);
+}
+
+#[test]
+fn test_complex_dot_product() {
+    let u = Vector::from(vec![Complex::new(0.0, 0.0), Complex::new(0.0, 0.0)]);
+    let v = Vector::from(vec![Complex::new(1.0, 1.0), Complex::new(1.0, 1.0)]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 0.0));
+
+    let u = Vector::from(vec![Complex::new(1.0, 1.0), Complex::new(1.0, 1.0)]);
+    let v = Vector::from(vec![Complex::new(1.0, 1.0), Complex::new(1.0, 1.0)]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 4.0));
+
+    let u = Vector::from(vec![Complex::new(-1.0, 6.0)]);
+    let v = Vector::from(vec![Complex::new(3.0, 2.0)]);
+    assert_eq!(u.dot(&v), Complex::new(-15.0, 16.0));
+
+    let u = Vector::from(vec![Complex::new(-1.0, 6.0), Complex::new(3.0, 9.0), Complex::new(0.0, 0.0), Complex::new(-9.0, -1.0)]);
+    let v = Vector::from(vec![Complex::new(3.0, 2.0), Complex::new(6.0, 3.0), Complex::new(-6.0, -6.0), Complex::new(-1.0, 0.0)]);
+    assert_eq!(u.dot(&v), Complex::new(-15.0, 80.0));
+
+    let u = Vector::from(vec![Complex::new(3.0, 9.0), Complex::new(3.0, -9.0)]);
+    let v = Vector::from(vec![Complex::new(6.0, 3.0), Complex::new(-6.0, -1.0)]);
+    assert_eq!(u.dot(&v), Complex::new(-36.0, 114.0));
+
+    let u = Vector::from(vec![Complex::new(0.0, 0.0), Complex::new(0.0, 0.0)]);
+    let v = Vector::from(vec![Complex::new(0.0, 0.0), Complex::new(0.0, 0.0)]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 0.0));
+
+    let u: Vector<Complex<f32>> = Vector::from(vec![]);
+    let v = Vector::from(vec![]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 0.0));
+    // undefined
+    let u: Vector<Complex<f32>> = Vector::from(vec![Complex::new(1.0, 0.0)]);
+    let v = Vector::from(vec![]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 0.0));
+
+    let u: Vector<Complex<f32>> = Vector::from(vec![]);
+    let v = Vector::from(vec![Complex::new(1.0, 0.0)]);
+    assert_eq!(u.dot(&v), Complex::new(0.0, 0.0));
 }
