b053a38a517c097ba78a1550126f8501f6f03a50
llecoq <lecoqlq@gmail.com>
2023-08-23 16:54:28 +0200

    Update `projection`

--- src/operations/projection_matrix.rs
+++ src/operations/projection_matrix.rs
@@ -3,30 +3,18 @@ use crate::Matrix;
 /// Computes a projection matrix to be used to render 3D objects.
 pub fn projection(fov: f32, ratio: f32, near: f32, far: f32) -> Matrix::<f32> {
 
-    // Compute the scale factor based on the field of view.
-    let scale = f32::tan(fov * 0.5) * near;
+    let scale_y: f32 = 1.0 / f32::tan(fov / 2.0);
+    let scale_x: f32 = scale_y / ratio;
+   
+    let a = (far + near) / (near - far);
+    let b = (2.0 * far * near) / (near - far);
 
-    // Compute the right and left, top and bottom values for the frustum.
-    let right = ratio * scale;
-    let left = -right;
-    let top = scale;
-    let bottom = -top;
-
-    // Initialize the projection matrix to all zeros.
-    let mut projection_matrix: Matrix<f32> = Matrix::from(vec![
-        vec![0.0,0.0,0.0,0.0],
-        vec![0.0,0.0,0.0,0.0],
-        vec![0.0,0.0,0.0,0.0],
-        vec![0.0,0.0,0.0,0.0],
+    let projection_matrix: Matrix<f32> = Matrix::from(vec![
+        vec![scale_x, 0., 0., 0.],
+        vec![0., scale_y, 0., 0.],
+        vec![0., 0., a, b],
+        vec![0., 0., -1., 0.]
     ]).unwrap();
-
-    // Assign values to the matrix based on the frustum.
-    projection_matrix[0][0] = (2.0 * near) / (right - left);
-    projection_matrix[1][1] = (2.0 * near) / (top - bottom);
-    projection_matrix[2][2] = -(far + near) / (far - near);
-    projection_matrix[2][3] = -1.0;
-    projection_matrix[3][2] = -(2.0 * far * near) / (far - near);
-
+   
     projection_matrix
-
 }
---
+++ src/tests/ex14_tests.rs
@@ -0,0 +1,6 @@
+use crate::projection;
+
+#[test]
+fn projection_matrix_tests() {
+    println!("{}", projection(0.785, 1., 0.1, 100.))
+}
