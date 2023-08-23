2d994e230e89b700df51abdaeab3b4332c251897
llecoq <lecoqlq@gmail.com>
2023-08-23 18:58:02 +0200

    Update `projection`

--- src/operations/projection_matrix.rs
+++ src/operations/projection_matrix.rs
@@ -2,19 +2,14 @@ use crate::Matrix;
 
 /// Computes a projection matrix to be used to render 3D objects.
 pub fn projection(fov: f32, ratio: f32, near: f32, far: f32) -> Matrix::<f32> {
-
-    let scale_y: f32 = 1.0 / f32::tan(fov / 2.0);
-    let scale_x: f32 = scale_y / ratio;
    
     let a = (far + near) / (near - far);
     let b = (2.0 * far * near) / (near - far);
 
-    let projection_matrix: Matrix<f32> = Matrix::from(vec![
-        vec![scale_x, 0., 0., 0.],
-        vec![0., scale_y, 0., 0.],
+    Matrix::from(vec![
+        vec![fov / ratio, 0., 0., 0.],
+        vec![0., fov, 0., 0.],
         vec![0., 0., a, b],
         vec![0., 0., -1., 0.]
-    ]).unwrap();
-   
-    projection_matrix
+    ]).unwrap()
 }
--- src/tests/ex14_tests.rs
+++ src/tests/ex14_tests.rs
@@ -2,5 +2,5 @@ use crate::projection;
 
 #[test]
 fn projection_matrix_tests() {
-    println!("{}", projection(0.785, 1., 0.1, 100.))
+    println!("{}", projection(45., 30. / 40., 25., 300.));
 }
