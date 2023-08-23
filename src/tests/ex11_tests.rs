467d06654cb770833de563ebaae8b6aa2c9e884f
llecoq <lecoqlq@gmail.com>
2023-08-23 13:32:40 +0200

    Update tests

--- src/tests/ex11_tests.rs
+++ src/tests/ex11_tests.rs
@@ -1,7 +1,9 @@
+use num_complex::Complex;
+
 use crate::Matrix;
 
 #[test]
-fn determinant_tests() {
+fn f32_determinant_tests() {
     let u = Matrix::from(vec![
         vec![ 1., -1.],
         vec![-1., 1.],
@@ -33,4 +35,35 @@ fn determinant_tests() {
     ]).unwrap();
     assert_eq!(u.determinant(), 1032.0);
     // 1032
+}
+
+#[test]
+fn complex_determinant_tests() {
+    let u = Matrix::from(vec![
+        vec![Complex::new(1.0, 1.0), Complex::new(-1.0, -1.0)],
+        vec![Complex::new(-1.0, 1.0), Complex::new(1.0, -1.0)]
+    ]).unwrap();
+    assert_eq!(u.determinant(), Complex::new(0.0, 0.0));
+
+    let u = Matrix::from(vec![
+        vec![Complex::new(2.0, 1.0), Complex::new(0.0, 0.0), Complex::new(0.0, 0.0)],
+        vec![Complex::new(0.0, 0.0), Complex::new(2.0, -1.0), Complex::new(0.0, 0.0)],
+        vec![Complex::new(0.0, 0.0), Complex::new(0.0, 0.0), Complex::new(2.0, 0.5)]
+    ]).unwrap();
+    assert_eq!(u.determinant(), Complex::new(10.0, 2.5));
+
+    let u = Matrix::from(vec![
+        vec![Complex::new(8.0, -3.0), Complex::new(5.0, 2.0), Complex::new(-2.0, 1.5)],
+        vec![Complex::new(4.0, 0.5), Complex::new(7.0, -0.5), Complex::new(20.0, 0.0)],
+        vec![Complex::new(7.0, 0.0), Complex::new(6.0, 1.5), Complex::new(1.0, -2.0)]
+    ]).unwrap();
+    assert_eq!(u.determinant(), Complex::new(-352.75, 229.875));
+
+    let u = Matrix::from(vec![
+        vec![Complex::new(8.0, 0.5), Complex::new(5.0, -0.5), Complex::new(-2.0, 0.0), Complex::new(4.0, -1.0)],
+        vec![Complex::new(4.0, 1.0), Complex::new(2.5, -0.5), Complex::new(20.0, 0.5), Complex::new(4.0, 0.0)],
+        vec![Complex::new(8.0, -1.5), Complex::new(5.0, 0.0), Complex::new(1.0, -2.0), Complex::new(4.0, 1.5)],
+        vec![Complex::new(28.0, 0.0), Complex::new(-4.0, -2.0), Complex::new(17.0, 0.0), Complex::new(1.0, -1.0)]
+    ]).unwrap();
+    assert_eq!(u.determinant(), Complex::new(1453.0, -8676.75));
 }
