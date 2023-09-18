ea62d7586c46348c6aa9edc363f563a0f5bc9d4d
llecoq <lecoqlq@gmail.com>
2023-09-18 11:16:38 +0200

    added doc

---
+++ target/doc/matrix/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["MatrixError"],"mod":["cosine","cross_product","linear_combination","linear_interpolation","matrix","operations","projection_matrix","traits","vector"],"struct":["Matrix","Vector"]};
